7d99a72581396af9c3daff3d3b9ec8b40c6dc67d
Sayma Rahman <saymarahmansara946@gmail.com>
2025-11-03 20:34:42 +0600

    Add files via upload

---
+++ sheet 3 F.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int N;
+    cin >> N;
+    long long A[N];
+
+    for (int i = 0; i < N; i++)
+        cin >> A[i];
+
+    for (int i = N - 1; i >= 0; i--)
+        {
+        cout << A[i];
+        if (i != 0) cout << " ";
+        }
+
+    return 0;
+}
+
